b668f5ea3f4f575f8d5d752b198dbc2b0cac530a
Vanessa Trezena <vanessa.trz.amorim@gmail.com>
2025-04-28 22:50:19 -0300

    atualização 2

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,7 +10,7 @@ export function Header() {
                 <Link href='/' className='nav-logo'>
                     <img src='/logo.png' alt='Logo' className='logo' />
                 </Link>
-                <strong>W3 finance</strong>
+                <strong>W2 finance</strong>
 
                 <ul className='nav-list'>
                     <li>
