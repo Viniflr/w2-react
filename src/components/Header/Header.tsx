64bcdd5f4a1b3f3e2b5ef6c34751e13baa6285fb
Vanessa Trezena <vanessa.trz.amorim@gmail.com>
2025-05-04 00:19:02 -0300

    continuação header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,29 @@
 import styles from './Header.module.css'
-import Link from 'next/Link'
-
-console.log(styles)
+import logoW2 from '../../img/logow2.svg'
+import logoEspecialista from '../../img/balao_chat.svg'
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <nav className='nav-bar'>
-                <Link href='/' className='nav-logo'>
-                    <img src='/logo.png' alt='Logo' className='logo' />
-                </Link>
-                <strong>W2 finance</strong>
+            <div className={styles.nav_bar}>
+                <img className={styles.logoW2} src={logoW2} alt="" />
 
-                <ul className='nav-list'>
-                    <li>
-                    </li>
-                </ul>
-            </nav>
+                <div className={styles.nav_list}>
+                    <a href="#">Home</a>
+                    <a href="#">Quem somos</a>
+                    <a href="#">Serviços</a>
+                    <a href="#">Noso Time</a>
+                    <a href="#">Contato</a>
+                </div>
+
+                <div className={styles.fale_com_especialista}>
+                    <button className={styles.header_button}>
+                        Fale com Especialista
+                        <img className={styles.logoEspecialista}  src={logoEspecialista} alt="" />
+                    </button>
+                </div>
+
+            </div>
         </header>
     );
 }
